666fab0c126baf0ac92d362f93ed0c09eb6c1bb4
njlyon0 <nicholasjlyon@gmail.com>
2023-05-15 07:09:18 -0700

    Tweaked extraction for actual layer names and tidied housekeeping a bit

--- extract-airtemp.R
+++ extract-airtemp.R
@@ -14,7 +14,10 @@
 # Read needed libraries
 # install.packages("librarian")
 librarian::shelf(tidyverse, sf, ncdf4, stars, terra, exactextractr, 
-                 NCEAS/scicomptools, googledrive)
+                 geojsonio, NCEAS/scicomptools, googledrive)
+
+# Silence `dplyr::summarize` preemptively
+options(dplyr.summarise.inform = FALSE)
 
 # Clear environment
 rm(list = ls())
@@ -26,12 +29,12 @@ rm(list = ls())
                               local_path = getwd()))
 
 # Load in GeoJSON file
-library(geojsonio)
-spdf <- geojsonio::geojson_read(x = file.path(path, "catchment-geojsons",
-                                              "four_corners_catchments.geojson"),  what = "sp")
+gjs_file <- geojsonio::geojson_read(x = file.path(path, "catchment-geojsons",
+                                                  "four_corners_catchments.geojson"),
+                                    what = "sp")
 
 # Convert to simple features object
-sf_file <- sf::st_as_sf(x = spdf)
+sf_file <- sf::st_as_sf(x = gjs_file)
 
 # Check result
 dplyr::glimpse(sf_file)
@@ -71,118 +74,121 @@ out_list <- list()
 
 # We'll need to strip each layer separately
 # for(k in 1:layer_ct){
-#   
-#   # Build name of layer
-#   focal_layer <- paste0("air_", k)
-#   
-#   # Rotate so longitude is from -180 to 180 (rather than 0 to 360)
-#   rotated <- terra::rotate(x = air_rast[[focal_layer]])
-#   
-#   # Identify time of this layer
-#   layer_time <- terra::time(x = rotated)
-#   
-#   # Strip out the relevant bit
-#   small_out_df <- exactextractr::exact_extract(x = rotated, y = sheds, 
-#                                                include_cols = c("river_id"),
-#                                                progress = F) %>%
-#     # Above returns a list so switch it to a dataframe
-#     purrr::map_dfr(dplyr::select, dplyr::everything()) %>%
-#     # Filter out NAs
-#     dplyr::filter(!is.na(value)) %>%
-#     # Convert from Kelvin to Celsius
-#     dplyr::mutate(value_c = value - 273.15) %>%
-#     # Average temperature within river ID
-#     dplyr::group_by(river_id) %>%
-#     dplyr::summarize(value_avg = mean(value_c, na.rm = T)) %>%
-#     dplyr::ungroup() %>%
-#     # Add a column for what timestamp this is
-#     dplyr::mutate(time = layer_time, .before = dplyr::everything())
-#   
-#   # Add it to the list
-#   out_list[[focal_layer]] <- small_out_df
-#   
-#   # Success message
-#   message("Processing complete for ", layer_time, " (number ", k, " of ", layer_ct, ")") }
+for(k in 1:2){
+  
+  # Build name of layer
+  focal_layer <- paste0("air_", k)
+  
+  # Rotate so longitude is from -180 to 180 (rather than 0 to 360)
+  rotated <- terra::rotate(x = air_rast[[focal_layer]])
+  
+  # Identify time of this layer
+  layer_time <- terra::time(x = rotated)
+  
+  # Strip out the relevant bit
+  small_out_df <- exactextractr::exact_extract(x = rotated, y = sf_file,
+                                               include_cols = c("usgs_site", "area_km2"),
+                                               progress = F) %>%
+    # Above returns a list so switch it to a dataframe
+    purrr::list_rbind(x = .) %>%
+    # Filter out NAs
+    dplyr::filter(!is.na(value)) %>%
+    # Convert from Kelvin to Celsius
+    dplyr::mutate(value_c = value - 273.15) %>%
+    # Average temperature within river ID
+    dplyr::group_by(usgs_site, area_km2) %>%
+    dplyr::summarize(value_avg = mean(value_c, na.rm = T)) %>%
+    dplyr::ungroup() %>%
+    # Add a column for what timestamp this is
+    dplyr::mutate(time = layer_time, 
+                  .before = dplyr::everything())
+  
+  # Add it to the list
+  out_list[[focal_layer]] <- small_out_df
+  
+  # Success message
+  message("Processing complete for ", layer_time, " (number ", k, " of ", layer_ct, ")") }
 
-# # Exploratory plot one of what we just extracted
-# plot(rotated, axes = T, reset = F)
-# plot(sheds, axes = T, add = T)
+# Exploratory plot one of what we just extracted
+plot(rotated, axes = T, reset = F)
+plot(sf_file["usgs_site"], axes = T, add = T)
 
 ## ------------------------------------------------------- ##
                   # Air Temp - Summarize ----
 ## ------------------------------------------------------- ##
-# # Unlist that list
-# full_out_df <- out_list %>%
-#   purrr::map_dfr(dplyr::select, dplyr::everything()) %>%
-#   # Strip out year and month
-#   dplyr::mutate(year = stringr::str_sub(string = time, start = 1, end = 4),
-#                 month = stringr::str_sub(string = time, start = 6, end = 7),
-#                 .after = time)
-# 
-# # Glimpse it
-# dplyr::glimpse(full_out_df)
-# 
-# # Summarize within month across years
-# year_df <- full_out_df %>%
-#   # Do summarization
-#   dplyr::group_by(river_id, year) %>%
-#   dplyr::summarize(value = mean(value_avg, na.rm = T)) %>%
-#   dplyr::ungroup() %>%
-#   # Make more informative year column
-#   dplyr::mutate(name = paste0("temp_", year, "_degC")) %>%
-#   # Drop simple year column
-#   dplyr::select(-year) %>%
-#   # Pivot to wide format
-#   tidyr::pivot_wider(names_from = name,
-#                      values_from = value)
-# 
-# # Glimpse this
-# dplyr::glimpse(year_df)
-# 
-# # Then summarize within year across months
-# month_df <- full_out_df %>%
-#   # Do summarization
-#   dplyr::group_by(river_id, month) %>%
-#   dplyr::summarize(value = mean(value_avg, na.rm = T)) %>%
-#   dplyr::ungroup() %>%
-#   # Change month number to letters
-#   dplyr::mutate(month_simp = dplyr::case_when(
-#     month == "01" ~ "jan",
-#     month == "02" ~ "feb",
-#     month == "03" ~ "mar",
-#     month == "04" ~ "apr",
-#     month == "05" ~ "may",
-#     month == "06" ~ "jun",
-#     month == "07" ~ "jul",
-#     month == "08" ~ "aug",
-#     month == "09" ~ "sep",
-#     month == "10" ~ "oct",
-#     month == "11" ~ "nov",
-#     month == "12" ~ "dec")) %>%
-#   # Make more informative month column
-#   dplyr::mutate(name = paste0("temp_", month_simp, "_degC")) %>%
-#   # Drop simple month column
-#   dplyr::select(-month, -month_simp) %>%
-#   # Pivot to wide format
-#   tidyr::pivot_wider(names_from = name,
-#                      values_from = value) %>%
-#   # Reorder months into chronological order
-#   dplyr::select(river_id, dplyr::contains("_jan_"), dplyr::contains("_feb_"),
-#                 dplyr::contains("_mar_"), dplyr::contains("_apr_"),
-#                 dplyr::contains("_may_"), dplyr::contains("_jun_"),
-#                 dplyr::contains("_jul_"), dplyr::contains("_aug_"),
-#                 dplyr::contains("_sep_"), dplyr::contains("_oct_"),
-#                 dplyr::contains("_nov_"), dplyr::contains("_dec_"))
-# 
-# # Glimpse this
-# dplyr::glimpse(month_df)
-# 
-# # Combine these dataframes
-# air_actual <- year_df %>%
-#   dplyr::left_join(y = month_df, by = "river_id")
-# 
-# # Glimpse again
-# dplyr::glimpse(air_actual)
+# Unlist that list
+full_out_df <- out_list %>%
+  purrr::list_rbind() %>%
+  # Strip out year and month
+  dplyr::mutate(year = stringr::str_sub(string = time, start = 1, end = 4),
+                month = stringr::str_sub(string = time, start = 6, end = 7),
+                .after = time)
+
+# Glimpse it
+dplyr::glimpse(full_out_df)
+
+# Summarize within month across years
+year_df <- full_out_df %>%
+  # Do summarization
+  dplyr::group_by(usgs_site, area_km2, year) %>%
+  dplyr::summarize(value = mean(value_avg, na.rm = T)) %>%
+  dplyr::ungroup() %>%
+  # Make more informative year column
+  dplyr::mutate(name = paste0("temp_", year, "_degC")) %>%
+  # Drop simple year column
+  dplyr::select(-year) %>%
+  # Pivot to wide format
+  tidyr::pivot_wider(names_from = name,
+                     values_from = value)
+
+# Glimpse this
+dplyr::glimpse(year_df)
+
+# Then summarize within year across months
+month_df <- full_out_df %>%
+  # Do summarization
+  dplyr::group_by(usgs_site, area_km2, month) %>%
+  dplyr::summarize(value = mean(value_avg, na.rm = T)) %>%
+  dplyr::ungroup() %>%
+  # Change month number to letters
+  dplyr::mutate(month_simp = dplyr::case_when(
+    month == "01" ~ "jan",
+    month == "02" ~ "feb",
+    month == "03" ~ "mar",
+    month == "04" ~ "apr",
+    month == "05" ~ "may",
+    month == "06" ~ "jun",
+    month == "07" ~ "jul",
+    month == "08" ~ "aug",
+    month == "09" ~ "sep",
+    month == "10" ~ "oct",
+    month == "11" ~ "nov",
+    month == "12" ~ "dec")) %>%
+  # Make more informative month column
+  dplyr::mutate(name = paste0("temp_", month_simp, "_degC")) %>%
+  # Drop simple month column
+  dplyr::select(-month, -month_simp) %>%
+  # Pivot to wide format
+  tidyr::pivot_wider(names_from = name,
+                     values_from = value) %>%
+  # Reorder months into chronological order
+  dplyr::select(usgs_site, area_km2, 
+                dplyr::contains("_jan_"), dplyr::contains("_feb_"),
+                dplyr::contains("_mar_"), dplyr::contains("_apr_"),
+                dplyr::contains("_may_"), dplyr::contains("_jun_"),
+                dplyr::contains("_jul_"), dplyr::contains("_aug_"),
+                dplyr::contains("_sep_"), dplyr::contains("_oct_"),
+                dplyr::contains("_nov_"), dplyr::contains("_dec_"))
+
+# Glimpse this
+dplyr::glimpse(month_df)
+
+# Combine these dataframes
+air_actual <- year_df %>%
+  dplyr::left_join(y = month_df, by = c("usgs_site", "area_km2"))
+
+# Glimpse again
+dplyr::glimpse(air_actual)
 
 ## ------------------------------------------------------- ##
                   # Air Temp - Export ----
