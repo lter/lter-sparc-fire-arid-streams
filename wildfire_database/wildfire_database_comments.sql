a974ac29bba054a46090eee884881c550e9cb542
Stevan Earl <stevan.earl@asu.edu>
2024-03-14 06:58:04 -0700

    docs: move wildfire db comments to separate sql file

---
+++ wildfire_database/wildfire_database_comments.sql
@@ -0,0 +1,5 @@
+-- comments on wildfire database
+
+COMMENT ON COLUMN firearea.water_chem."ActivityStartTime.Time" IS 'local time' ;
+COMMENT ON COLUMN firearea.water_chem."ActivityStartDateTime" IS 'UTC' ;
+COMMENT ON COLUMN firearea.discharge_daily."Flow" IS 'cubic feet per second (cfs)' ;
