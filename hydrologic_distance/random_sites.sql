3d0a5c3e3cb41ed44417025e8315a4fb80688604
Stevan Earl <stevan.earl@asu.edu>
2025-10-05 15:16:33 -0700

    feat: add SQL script to select random distinct USGS sites based on flowline counts

---
+++ hydrologic_distance/random_sites.sql
@@ -0,0 +1,34 @@
+-- WITH all_sites AS (
+-- SELECT usgs_site FROM firearea.largest_ammonium_valid_fire_per_site UNION ALL
+-- SELECT usgs_site FROM firearea.largest_nitrate_valid_fire_per_site UNION ALL
+-- SELECT usgs_site FROM firearea.largest_orthop_valid_fire_per_site UNION ALL
+-- SELECT usgs_site FROM firearea.largest_spcond_valid_fire_per_site
+-- )
+-- SELECT *
+-- FROM (
+--     SELECT DISTINCT lower(usgs_site) AS usgs_site
+--     FROM all_sites
+-- ) AS distinct_sites
+-- ORDER BY RANDOM()
+-- LIMIT 3;
+
+WITH all_sites AS (
+  SELECT usgs_site FROM firearea.largest_ammonium_valid_fire_per_site UNION ALL
+  SELECT usgs_site FROM firearea.largest_nitrate_valid_fire_per_site UNION ALL
+  SELECT usgs_site FROM firearea.largest_orthop_valid_fire_per_site UNION ALL
+  SELECT usgs_site FROM firearea.largest_spcond_valid_fire_per_site
+),
+counts AS (
+  SELECT LOWER(usgs_site) as usgs_site
+  FROM firearea.flowlines
+  GROUP BY usgs_site
+  HAVING COUNT(nhdplus_comid) <= 300
+)
+SELECT distinct_sites.usgs_site
+FROM (
+    SELECT DISTINCT LOWER(all_sites.usgs_site) AS usgs_site
+    FROM all_sites
+    JOIN counts ON LOWER(all_sites.usgs_site) = counts.usgs_site
+) AS distinct_sites
+ORDER BY RANDOM()
+LIMIT 3;
